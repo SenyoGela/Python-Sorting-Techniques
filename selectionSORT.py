289a1d941d67ff9dc6031ac004dd7c9be80cdf2e
SenyoGela <senogelamariconb@gmail.com>
2023-01-26 02:05:37 +0800

    add if statement to find minimum value

--- selectionSORT.py
+++ selectionSORT.py
@@ -4,6 +4,7 @@ def sort(nums):
     for i in range(9):
         minpos = i
         for j in range(i,10):
-
+            if nums[j] < nums[minpos]:
+                minpos = j
 
 nums = [75, 30, 49, 81, 54, 57, 79, 50, 85, 71]

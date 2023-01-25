56807c41cd74966a3bf5470376da444c94fbd343
SenyoGela <senogelamariconb@gmail.com>
2023-01-26 04:26:11 +0800

    add while loop

--- quickSORT.py
+++ quickSORT.py
@@ -11,6 +11,12 @@ def partition(nums, left, right):
     j = right - 1
     pivot = nums[right]
 
+    while i < j:
+        while i < right and nums[i] < pivot:
+            i += 1
+        while j > left and nums[j] >= pivot
+            j -= 1
+
 
 nums = [75, 30, 49, 81, 54, 57, 79, 50, 85, 71]
 sort(nums)

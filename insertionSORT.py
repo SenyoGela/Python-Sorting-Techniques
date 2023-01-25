85900cad2de481a605755016995512f575b29864
SenyoGela <senogelamariconb@gmail.com>
2023-01-26 03:14:08 +0800

    add while loop for swapping

--- insertionSORT.py
+++ insertionSORT.py
@@ -3,7 +3,9 @@
 def sort(nums):
     for i in range(1, len(nums)):
         j = i
-
+        while nums[j-1] > nums[j]:
+            nums[j-1], nums[j] = nums[j], nums[j-1]
+            j -= 1
 
 nums = [75, 30, 49, 81, 54, 57, 79, 50, 85, 71]
 sort(nums)

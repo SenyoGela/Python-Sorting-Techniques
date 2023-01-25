fb7615621da0bfbc0052f8607310f8545510f0d2
SenyoGela <senogelamariconb@gmail.com>
2023-01-26 03:45:14 +0800

    merge array using while and if

--- mergeSORT.py
+++ mergeSORT.py
@@ -5,8 +5,24 @@ def sort(nums):
         left_nums = nums[:len(nums)//2]
         right_nums = nums[len(nums)//2:]
 
+        # recursion
         sort(left_nums)
         sort(right_nums)
+
+        # merge
+        i = 0   # left nums index
+        j = 0   # right nums index
+        k = 0   # merged array index
+        while i < len(left_nums) and j < len(right_nums):
+            if left_nums[i] < right_nums[j]:
+                nums[k] = left_nums[i]
+                i += 1
+                k += 1
+            else:
+                nums[k] = right_nums[j]
+                j += 1
+                k += 1
+
 
 nums = [75, 30, 49, 81, 54, 57, 79, 50, 85, 71]
 sort(nums)

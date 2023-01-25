7d8a7d113632fbe81e41921aeb6c1fb98c645e04
SenyoGela <senogelamariconb@gmail.com>
2023-01-26 04:20:00 +0800

    call partition function

--- quickSORT.py
+++ quickSORT.py
@@ -1,6 +1,11 @@
 # QUICK SORT
 
 def sort(nums, left, right):
+    if left < right:
+        partition_pos = partition(nums, left, right)
+        sort(nums, left, partition_pos - 1)
+        sort(nums, partition_pos + 1, right)
+
 
 
 nums = [75, 30, 49, 81, 54, 57, 79, 50, 85, 71]

7a35a5f9e03cb864973fe990adbd9ed351707328
SenyoGela <senogelamariconb@gmail.com>
2023-01-26 03:38:17 +0800

    call merge sort for both left and right array

--- mergeSORT.py
+++ mergeSORT.py
@@ -5,6 +5,8 @@ def sort(nums):
         left_nums = nums[:len(nums)//2]
         right_nums = nums[len(nums)//2:]
 
+        sort(left_nums)
+        sort(right_nums)
 
 nums = [75, 30, 49, 81, 54, 57, 79, 50, 85, 71]
 sort(nums)
